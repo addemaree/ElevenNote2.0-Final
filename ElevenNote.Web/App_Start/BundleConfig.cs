fa433695dad5e5d4d97e70da969845b3c741e918
Adam Demaree <demareeadam1@gmail.com>
2017-05-03 07:29:38 -0400

    Landing Page complete

--- ElevenNote.Web/App_Start/BundleConfig.cs
+++ ElevenNote.Web/App_Start/BundleConfig.cs
@@ -24,8 +24,8 @@ namespace ElevenNote.Web
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/css/bootstrap.css",
+                      "~/Content/css/Site.css"));
         }
     }
 }
